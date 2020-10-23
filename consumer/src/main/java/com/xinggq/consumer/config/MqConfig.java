49b3b7ff117088e3a44234f150a1b9c51cce9186
xyf-win <xuyfh@yonyou.com>
2020-10-23 15:57:45 +0800

    配置jdbc

--- consumer/src/main/java/com/xinggq/consumer/config/MqConfig.java
+++ consumer/src/main/java/com/xinggq/consumer/config/MqConfig.java
@@ -1,5 +1,8 @@
 package com.xinggq.consumer.config;
 
+import org.springframework.beans.factory.annotation.Configurable;
+
+@Configurable
 public class MqConfig {
 
 }
--- producer/src/main/java/com/xinggq/producer/config/MqConfig.java
+++ producer/src/main/java/com/xinggq/producer/config/MqConfig.java
@@ -1,5 +1,8 @@
 package com.xinggq.producer.config;
 
+import org.springframework.beans.factory.annotation.Configurable;
+
+@Configurable
 public class MqConfig {
 
 }
