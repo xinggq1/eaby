f750fe46c78f13ebcc96f2c732c2e77279a3fb19
xyf-win <xuyfh@yonyou.com>
2020-10-28 17:51:39 +0800

    全部提交一下，

--- producer/src/main/java/com/xinggq/producer/service/MessageProducer.java
+++ producer/src/main/java/com/xinggq/producer/service/MessageProducer.java
@@ -19,12 +19,13 @@ public class MessageProducer {
   @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
   public void processA(){
     //写入业务逻辑
-    log.info("写入业务逻辑");
+    log.info("写入业务逻辑,修改本地数据库");
     //记录消息表
     log.info("开始记录本地消息表");
     Message message = new Message("这个是消息内容");
     messageService.insert(message);
     //发送到消息队列
+
   }
 
 
